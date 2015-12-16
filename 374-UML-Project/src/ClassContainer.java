674be20f0360b2c9442fe8ea47e272c2cee45f52
srasp413 <srasp413@gmail.com>
2015-12-16 18:35:52 -0500

    Updated ClassContainer with addField and addMethod methods

--- 374-UML-Project/src/ClassContainer.java
+++ 374-UML-Project/src/ClassContainer.java
@@ -24,4 +24,40 @@ public class ClassContainer {
 		}
 		return null;
 	}
+	
+	void addField(String fieldName, String className, String access, String type) {
+		JClass currentClass = null;
+		JClass currentType = null;
+		for(JClass c: this.classes) {
+			if(c.getName().equals(className)) {
+				currentClass = c;
+				break;
+			}
+		}
+		for(JClass j: this.classes) {
+			if(j.getName().equals(type)) {
+				currentType = j;
+			}
+		}
+		currentClass.addField(new JField(fieldName, access, currentType));
+	}
+	
+	void addMethod(String methodName, String className, String access, ArrayList<String> parameters) {
+		JClass currentClass = null;
+		ArrayList<JClass> new_parameters = null;
+		for(JClass c: this.classes) {
+			if(c.getName().equals(className)) {
+				currentClass = c;
+				break;
+			}
+		}
+		for(JClass j: this.classes) {
+			for(String s: parameters) {
+				if(j.getName().equals(s)) {
+					new_parameters.add(j);
+				}
+			}
+		}
+		currentClass.addMethod(new JMethod(methodName, access, new_parameters));
+	}
 }
