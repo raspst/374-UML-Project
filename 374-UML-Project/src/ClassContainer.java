e941273c81ac014cd3a129b0bf99282afc70932b
unknown <crowleaj@CROWLEAJ-1.rose-hulman.edu>
2016-01-08 00:51:16 -0500

    Merged

--- 374-UML-Project/src/ClassContainer.java
+++ 374-UML-Project/src/ClassContainer.java
@@ -29,8 +29,6 @@ public class ClassContainer {
 		name = packages[packages.length-1];
 		packages = name.split("/");
 		name = packages[packages.length-1];
-		packages = name.split("\\.");
-		name = packages[packages.length-1];
 		//if(packages.length==1&&name!="void"&&name!="int"&&name!="float"&&name!="double"&&name!="boolean"&&name!="short"&&name!="byte"&&name!="char"&&name!="long")name = name.substring(1);
 		JClass theclass = classes.get(name);
 		if (theclass == null) {
