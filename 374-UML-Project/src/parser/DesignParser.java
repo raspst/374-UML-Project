9b2379499a5b5448fc451427c252d0ca1b54802d
Crowley <crowleaj@rose-hulman.edu>
2016-01-15 00:08:30 -0500

    Separated printing from parsing

--- 374-UML-Project/src/parser/DesignParser.java
+++ 374-UML-Project/src/parser/DesignParser.java
@@ -11,16 +11,8 @@ public class DesignParser {
 		ClassContainer container = new ClassContainer();
 		PrintFactory pf = new PrintFactory(container, args);
 		container.whitelist("parser/");
-		JClass c;
 		parseDesign(args, container);
-		System.out.println(pf.printInheritance());
-		System.out.println("edge [style = dotted]");
-		System.out.println(pf.printImplements());
-		System.out.println("edge [arrowhead = vee]");
-		System.out.println(pf.printAssociates());
-		System.out.println("edge [style = solid]");
- 		System.out.println(pf.printUses());
-		System.out.println("}");
+		pf.printContainer();
 		/*
 		 * ArrayList<JInterface> interfaces = new ArrayList<JInterface>();
 		 * interfaces.add(new JInterface("Interface1")); JClass testClass = new
@@ -33,12 +25,6 @@ public class DesignParser {
 	}
 
 	public static void parseDesign(String[] args, ClassContainer container) throws IOException {
-		JClass c;
-		System.out.println("digraph G {\nfontname = \"Bitstream Vera Sans\"\n" + "fontsize = 8\n"
-
-				+ "node [\nfontname = \"Bitstream Vera Sans\"fontsize = 8\nshape = \"record\"\n]" +
-
-		"edge [\nfontname = \"Bitstream Vera Sans\"\nfontsize = 8\n]");
 		for (String className : args) {
 //			System.out.println(className);
 			ClassReader reader = new ClassReader(className);
@@ -47,8 +33,6 @@ public class DesignParser {
 			ClassVisitor methodVisitor = new ClassMethodVisitor(Opcodes.ASM5, fieldVisitor, container);
 			reader.accept(methodVisitor, ClassReader.EXPAND_FRAMES);
 //			System.out.println(className);
-			c = container.getClass(className);
-			System.out.println(c.getGraphViz());
 		}
 	}
 }
--- 374-UML-Project/src/parser/PrintFactory.java
+++ 374-UML-Project/src/parser/PrintFactory.java
@@ -1,4 +1,5 @@
 package parser;
+
 import java.util.Iterator;
 
 public class PrintFactory {
@@ -10,6 +11,32 @@ public class PrintFactory {
 		classes = args;
 	}
 
+	public void printContainer() {
+		System.out.println("digraph G {\nfontname = \"Bitstream Vera Sans\"\n" + "fontsize = 8\n"
+
+				+ "node [\nfontname = \"Bitstream Vera Sans\"fontsize = 8\nshape = \"record\"\n]" +
+
+		"edge [\nfontname = \"Bitstream Vera Sans\"\nfontsize = 8\n]");
+		System.out.println(printGraphViz());
+		System.out.println(printInheritance());
+		System.out.println("edge [style = dotted]");
+		System.out.println(printImplements());
+		System.out.println("edge [arrowhead = vee]");
+		System.out.println(printAssociates());
+		System.out.println("edge [style = solid]");
+ 		System.out.println(printUses());
+		System.out.println("}");
+	}
+
+	public String printGraphViz(){
+		StringBuilder sb = new StringBuilder();
+		for (String name : classes) {
+			JClass c = container.getClass(name);
+			sb.append(c.getGraphViz()+'\n');
+		}
+		return sb.toString();
+	}
+	
 	public String printInheritance() {
 		StringBuilder sb = new StringBuilder();
 		for (String className : classes) {
@@ -55,7 +82,7 @@ public class PrintFactory {
 			JClass c = container.getClass(className);
 			for (JClass cl : c.getUses().values()) {
 				if (container.isWhitelisted(cl))
-					if(!c.getAssociates().contains(cl)) {
+					if (!c.getAssociates().contains(cl)) {
 						sb.append(c.getTopName() + "->" + cl.getTopName() + "\n");
 					}
 			}
