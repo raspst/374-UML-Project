dbc390a81807baff33be75a6e8c1921d9e9b4194
Crowley <crowleaj@rose-hulman.edu>
2016-01-27 23:17:56 -0500

    Method parsing works except for local variable error.

--- 374-UML-Project/src/uml/node/NodeContainer.java
+++ 374-UML-Project/src/uml/node/NodeContainer.java
@@ -52,11 +52,10 @@ public class NodeContainer {
 	}
 
 	public JClass getClass(String name) {
-		System.out.println(name);
 		if(Type.getObjectType(name)!=null)
-			name = Type.getObjectType(name).getInternalName();
+			name = Type.getObjectType(name).getClassName();
 		name = name.replace('.', '/');
-		System.out.println(name);
+		name = name.replace("[]", "");
 		JClass theClass = classes.get(name);
 		if (theClass == null) {
 			addClass(name);
@@ -69,26 +68,26 @@ public class NodeContainer {
 		try {
 			while (!toParse.isEmpty()) {
 				ClassReader cr;
+				System.out.println(toParse.peek());
 				cr = new ClassReader(toParse.remove());
 				ClassNode classNode = new ClassNode();
 				cr.accept(classNode, 0);
-				JClass c = getClass(classNode.name);
+				JClass c = getClass(Type.getObjectType(classNode.name).getClassName());
 				//must be java/lang/Object
 				if (classNode.superName == null)
 					c.setSuper(c);
 				else
-					c.setSuper(getClass(classNode.superName));
+					c.setSuper(getClass(Type.getObjectType(classNode.superName).getClassName()));
 				// if(classNode.interfaces!=null)
 				for (String n : (List<String>) classNode.interfaces) {
-					System.out.println(n);
-					JClass cl = getClass(n);
+					JClass cl = getClass(Type.getObjectType(classNode.name).getClassName());
 					cl.setInterface(true);
 					c.addInterface(cl);
 				}
 				System.out.println(c.getName());
 				System.out.println(c.getSuper().getName());
-				for (JInterface i : c.getInterfaces())
-					System.out.println(i.getTopName());
+//				for (JInterface i : c.getInterfaces())
+//					System.out.println(i.getTopName());
 				for (MethodNode method : (List<MethodNode>) classNode.methods) {
 					// System.out.println(method.signature);
 					InsnList insns = method.instructions;
@@ -111,28 +110,28 @@ public class NodeContainer {
 						localVars.add(new JField("this", 0, c));
 						++i;
 					}
-					System.out.println(len);
+					//System.out.println(len);
 					//if(len>0)System.out.println(argTypes[0]);
 					//System.out.println(vars.size());
 					//System.out.println(method.desc);
 					if(vars!=null&&len<=vars.size())
 					for (; i < len; ++i) {
 						LocalVariableNode n = vars.get(i);
-						localVars.add(new JField(n.name, 1, getClass(n.desc)));
-						params.add(new JField(n.name, 1, getClass(n.desc)));
+						localVars.add(new JField(n.name, 1, getClass(Type.getType(n.desc).getClassName())));
+						params.add(new JField(n.name, 1, getClass(Type.getType(n.desc).getClassName())));
 					}
 					int sz = 0;
 					if(vars!=null)sz=vars.size();
 					//if(len==0)len+=1;
 					for (i = len; i < sz; ++i) {
 						LocalVariableNode n = vars.get(i);
-						localVars.add(new JField(n.name, 2, getClass(n.desc)));
+						localVars.add(new JField(n.name, 2, getClass(Type.getType(n.desc).getClassName())));
 					}
 					JMethod m = new JMethod(c, method.name, method.access,
 							getClass(Type.getReturnType(method.desc).getClassName()), params, localVars, method.desc);
 					System.out.println(m.getAccess() + " " + m.getReturn().getTopName() + "    " + m.getName());
-					for (JField f : m.getLocalVars())
-						System.out.println(f.getName() + "    " + f.getType().getTopName());
+//					for (JField f : m.getLocalVars())
+//						System.out.println(f.getName() + "    " + f.getType().getTopName());
 //					for (LocalVariableNode var : (List<LocalVariableNode>) method.localVariables) {
 //						// System.out.println(var.name + " " + var.desc);
 //					}
